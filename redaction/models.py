3fbc19b7a95357e08b90215e4026041581be4c96
Olha Shumska <olgashumska65@gmail.com>
2025-09-27 08:47:56 +0200

    create model Newspaper

--- redaction/models.py
+++ redaction/models.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -20,3 +23,29 @@ class Redactor(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()
+    published_date = models.DateTimeField(default=datetime.now)
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="topics_newspapers"
+    )
+    sub_topic = models.ManyToManyField(
+        Topic,
+        blank=True,
+        related_name="subtopics_newspapers",
+    )
+    publishers = models.ManyToManyField(
+        get_user_model(),
+        related_name="newspapers"
+    )
+
+    def __str__(self):
+        return f"{self.topic.name}: {self.title}"
+
+    class Meta:
+        ordering = ("published_date", )
