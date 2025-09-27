50f0abec9a96e7798285f463df6f59772a78d5ef
Olha Shumska <olgashumska65@gmail.com>
2025-09-27 08:34:56 +0200

    create model Redactor

--- redaction/models.py
+++ redaction/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class Topic(models.Model):
@@ -9,3 +10,13 @@ class Topic(models.Model):
 
     class Meta:
         ordering = ("name", )
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ("username", )
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
