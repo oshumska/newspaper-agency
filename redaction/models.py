d11f3c28572c42949831fb6deeb49c70db67cfed
Olha Shumska <olgashumska65@gmail.com>
2025-09-27 08:32:32 +0200

    create model named Topic

--- redaction/models.py
+++ redaction/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("name", )
