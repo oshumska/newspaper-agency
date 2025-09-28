f3453d547d4dd8e121d5446911bd1f245054c30f
Olha Shumska <olgashumska65@gmail.com>
2025-09-28 16:33:37 +0200

    register models in admin panel

--- redaction/admin.py
+++ redaction/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from redaction.models import Topic, Redactor, Newspaper
+
+admin.site.register(Topic)
+admin.site.register(Redactor)
+admin.site.register(Newspaper)
