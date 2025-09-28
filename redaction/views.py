618f7d98cc61274f1e68ffac5a44bcbf76bc3041
Olha Shumska <olgashumska65@gmail.com>
2025-09-28 16:26:39 +0200

    create index function for home page

--- redaction/views.py
+++ redaction/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from redaction.models import Topic, Redactor, Newspaper
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_topic = Topic.objects.count()
+    num_redactor = Redactor.objects.count()
+    num_newspaper = Newspaper.objects.count()
+    context = {
+        "num_topic": num_topic,
+        "num_redactor": num_redactor,
+        "num_newspaper": num_newspaper,
+    }
+    return render(request, "redaction/index.html", context=context)
