e0ebb5a8c576d333c6010af7ae042c7bbbb5a486
Olha Shumska <olgashumska65@gmail.com>
2025-09-29 09:02:55 +0200

    create path for topic list

--- redaction/urls.py
+++ redaction/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from redaction.views import index
+from redaction.views import index, TopicListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("topic/", TopicListView.as_view(), name="topic-list")
 ]
 
 app_name = "redaction"
