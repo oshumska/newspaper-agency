5fe840cc226f341957ead664ecdd9a90d742d956
Olha Shumska <olgashumska65@gmail.com>
2025-09-29 09:24:16 +0200

    add redactor list view to path

--- redaction/urls.py
+++ redaction/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from redaction.views import index, TopicListView
+from redaction.views import index, TopicListView, RedactorListView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("topic/", TopicListView.as_view(), name="topic-list")
+    path("topic/", TopicListView.as_view(), name="topic-list"),
+    path("redactor/", RedactorListView.as_view(), name="redactor-list"),
 ]
 
 app_name = "redaction"
