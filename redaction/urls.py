427e8f7f21c871a2e6a884c98648dc5ece173cab
Olha Shumska <olgashumska65@gmail.com>
2025-09-29 17:50:24 +0200

    create topic update view and add it to the path

--- redaction/urls.py
+++ redaction/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from redaction.views import index, TopicListView, RedactorListView, NewspaperListView, TopicCreateView
+from redaction.views import (
+    index,
+    TopicListView,
+    RedactorListView,
+    NewspaperListView,
+    TopicCreateView,
+    TopicUpdateView
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("topic/", TopicListView.as_view(), name="topic-list"),
     path("topic/create/", TopicCreateView.as_view(), name="topic-create"),
+    path("topic/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update"),
     path("redactor/", RedactorListView.as_view(), name="redactor-list"),
     path("newspaper/", NewspaperListView.as_view(), name="newspaper-list"),
 ]
--- redaction/views.py
+++ redaction/views.py
@@ -29,6 +29,12 @@ class TopicCreateView(generic.CreateView):
     success_url = reverse_lazy("redaction:topic-list")
 
 
+class TopicUpdateView(generic.UpdateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("redaction:topic-list")
+
+
 class RedactorListView(generic.ListView):
     model = Redactor
     paginate_by = 10
