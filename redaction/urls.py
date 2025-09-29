87d120ee86e93710e0a504393fc7f0539849ad4c
Olha Shumska <olgashumska65@gmail.com>
2025-09-29 09:31:30 +0200

    add newspaper list to path

--- redaction/urls.py
+++ redaction/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from redaction.views import index, TopicListView, RedactorListView
+from redaction.views import index, TopicListView, RedactorListView, NewspaperListView
 
 urlpatterns = [
     path("", index, name="index"),
     path("topic/", TopicListView.as_view(), name="topic-list"),
     path("redactor/", RedactorListView.as_view(), name="redactor-list"),
+    path("newspaper/", NewspaperListView.as_view(), name="newspaper-list"),
 ]
 
 app_name = "redaction"
