171e11560e65a14a75419ed35a6bb4c47332b7cf
Olha Shumska <olgashumska65@gmail.com>
2025-09-29 17:44:50 +0200

    create path for topic create

--- redaction/urls.py
+++ redaction/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from redaction.views import index, TopicListView, RedactorListView, NewspaperListView
+from redaction.views import index, TopicListView, RedactorListView, NewspaperListView, TopicCreateView
 
 urlpatterns = [
     path("", index, name="index"),
     path("topic/", TopicListView.as_view(), name="topic-list"),
+    path("topic/create/", TopicCreateView.as_view(), name="topic-create"),
     path("redactor/", RedactorListView.as_view(), name="redactor-list"),
     path("newspaper/", NewspaperListView.as_view(), name="newspaper-list"),
 ]
