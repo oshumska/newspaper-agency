413956e9b65669bbd4cf9b85c979f80627d37be7
Olha Shumska <olgashumska65@gmail.com>
2025-09-30 17:57:07 +0200

    add path to redactor detail view to urlpatterns

--- redaction/urls.py
+++ redaction/urls.py
@@ -7,7 +7,7 @@ from redaction.views import (
     TopicCreateView,
     TopicUpdateView,
     RedactorCreateView,
-    NewspaperCreateView,
+    NewspaperCreateView, RedactorDetailView,
 )
 
 urlpatterns = [
@@ -16,6 +16,7 @@ urlpatterns = [
     path("topic/create/", TopicCreateView.as_view(), name="topic-create"),
     path("topic/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update"),
     path("redactor/", RedactorListView.as_view(), name="redactor-list"),
+    path("redactor/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
     path("redactor/create/", RedactorCreateView.as_view(), name="redactor-create"),
     path("newspaper/", NewspaperListView.as_view(), name="newspaper-list"),
     path("newspaper/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
