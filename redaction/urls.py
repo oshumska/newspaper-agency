c1f71de82e06c962ab4dd27f72c20aa351e43bef
Olha Shumska <olgashumska65@gmail.com>
2025-10-03 13:09:52 +0200

    add update redactor experience to urlpatterns

--- redaction/urls.py
+++ redaction/urls.py
@@ -7,7 +7,11 @@ from redaction.views import (
     TopicCreateView,
     TopicUpdateView,
     RedactorCreateView,
-    NewspaperCreateView, RedactorDetailView, NewspaperDetailView, NewspaperUpdateView,
+    NewspaperCreateView,
+    RedactorDetailView,
+    NewspaperDetailView,
+    NewspaperUpdateView,
+    RedactorUpdateYearsOfExperience,
 )
 
 urlpatterns = [
@@ -17,6 +21,7 @@ urlpatterns = [
     path("topic/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update"),
     path("redactor/", RedactorListView.as_view(), name="redactor-list"),
     path("redactor/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
+    path("redactor/<int:pk>/update-experience/", RedactorUpdateYearsOfExperience.as_view(), name="update-experience"),
     path("redactor/create/", RedactorCreateView.as_view(), name="redactor-create"),
     path("newspaper/", NewspaperListView.as_view(), name="newspaper-list"),
     path("newspaper/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
