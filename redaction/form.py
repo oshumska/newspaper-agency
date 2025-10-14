ad6850e851d9127e6ca038b8ff0eda6a1e1eb90d
Olha Shumska <olgashumska65@gmail.com>
2025-10-14 14:33:08 +0200

    create redactor search form

--- redaction/form.py
+++ redaction/form.py
@@ -90,3 +90,14 @@ class NewspaperSearchForm(forms.Form):
             "placeholder": "Search by title",
         }),
     )
+
+
+class RedactorSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={
+            "placeholder": "Search by username",
+        }),
+    )
