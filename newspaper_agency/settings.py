cbb93790a6b169be3d11c07afc5affef04c8bef8
Olha Shumska <olgashumska65@gmail.com>
2025-10-22 08:42:28 +0200

    remove secret key from setting file and save it in env file create file .env.sample according to .env file

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nu7-)wwqi^!=pt-w!(dh^j-a62^xbpohv5a9o2%=l-t*@@at02"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
