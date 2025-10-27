9302db00793016c05deba97abb981fdc448965e8
Olha Shumska <olgashumska65@gmail.com>
2025-10-27 15:32:15 +0100

    fix redactor user in admin panel add years_of_experience field to create redactor user add few features for Newspapers to admin panel

--- redaction/admin.py
+++ redaction/admin.py
@@ -1,7 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from redaction.models import Topic, Redactor, Newspaper
 
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "published_date",
+        "topic",
+    ]
+    list_filter = [
+        "topic",
+        "published_date",
+    ]
+    search_fields = [
+        "title",
+    ]
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info",
+            {"fields": ("years_of_experience",)},
+        ),
+    )
+
+
 admin.site.register(Topic)
-admin.site.register(Redactor)
-admin.site.register(Newspaper)
